0dcd5fab4cbad84588ca1a4f5701829b4fc9d390
Vladislav Filipov <vlad1philip@gmail.com>
2023-02-16 04:28:04 +0500

    webpack config args

--- webpack.config.js
+++ webpack.config.js
@@ -8,82 +8,86 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const PRODUCTION = "production",
   DEVELOPMENT = "development";
 
-let mode = DEVELOPMENT;
-let target = "web";
-if (process.env.NODE_ENV === PRODUCTION) {
-  mode = PRODUCTION;
-  target = "browserslist";
-}
+module.exports = (env) => {
+  console.log("env", env);
+  let mode = DEVELOPMENT;
+  let target = "web";
 
-module.exports = {
-  mode,
-  target,
-  entry: "./src/index.tsx",
-  devtool: "source-map",
-  output: {
-    filename: "[name].bundle.js",
-    chunkFilename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
-    assetModuleFilename: "assets/[hash][ext][query]",
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.m?[jt]sx?$/,
-        exclude: "/node_modules/",
-        use: {
-          loader: "babel-loader",
+  if (env.mode === PRODUCTION) {
+    mode = PRODUCTION;
+    target = "browserslist";
+  }
+
+  return {
+    mode,
+    target,
+    entry: "./src/index.tsx",
+    devtool: "source-map",
+    output: {
+      filename: "[name].bundle.js",
+      chunkFilename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      clean: true,
+      assetModuleFilename: "assets/[hash][ext][query]",
+      publicPath: "/",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
         },
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
-        type: "asset/resource",
-      },
+        {
+          test: /\.m?[jt]sx?$/,
+          exclude: "/node_modules/",
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg|webp|ico)$/i,
+          type: "asset/resource",
+        },
+      ],
+    },
+    resolve: {
+      // alias: {
+      //   "@": path.resolve(__dirname, "./src"),
+      // },
+      plugins: [new TsconfigPathsPlugin()],
+      extensions: [
+        ".ts",
+        ".tsx",
+        ".scss",
+        ".css",
+        ".webpack.js",
+        ".web.js",
+        ".js",
+      ],
+    },
+    plugins: [
+      new Dotenv(),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename:
+          mode === DEVELOPMENT ? "[name].css" : "[name].[contenthash].css",
+      }),
     ],
-  },
-  resolve: {
-    // alias: {
-    //   "@": path.resolve(__dirname, "./src"),
-    // },
-    plugins: [new TsconfigPathsPlugin()],
-    extensions: [
-      ".ts",
-      ".tsx",
-      ".scss",
-      ".css",
-      ".webpack.js",
-      ".web.js",
-      ".js",
-    ],
-  },
-  plugins: [
-    new Dotenv(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/index.html",
-    }),
-    new MiniCssExtractPlugin({
-      filename:
-        mode === DEVELOPMENT ? "[name].css" : "[name].[contenthash].css",
-    }),
-  ],
-  devServer: {
-    static: "./dist",
-    historyApiFallback: true,
-  },
-  optimization: {
-    nodeEnv: DEVELOPMENT,
-  },
+    devServer: {
+      static: "./dist",
+      historyApiFallback: true,
+    },
+    optimization: {
+      nodeEnv: DEVELOPMENT,
+    },
+  };
 };
