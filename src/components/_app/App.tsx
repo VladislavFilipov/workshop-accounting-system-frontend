90117fccc891704793f1dbaf9d7627cad0c8fc49
Vladislav Filipov <vlad1philip@gmail.com>
2023-03-03 00:51:24 +0500

    test pre-commit scss

--- src/components/_app/App.tsx
+++ src/components/_app/App.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import { RouterProvider, RouterProviderProps } from "react-router-dom";
 
-const App: FC<{ router: RouterProvderProps["router"] }> = ({ router }) => {
+const App: FC<{ router: RouterProviderProps["router"] }> = ({ router }) => {
   return (
     <div className="App">
       123
--- src/tests/_helpers.tsx
+++ src/tests/_helpers.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import { createMemoryRouter } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
